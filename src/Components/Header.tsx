efb97fc56ead7b94a5694144a26fbff98865ba28
muzammildha <info.allmaster@gmail.com>
2025-01-18 19:47:51 +0500

    Template 7

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,19 +7,19 @@ export default function Header() {
       {/* Logo */}
       <div className="w-[120px] h-[40px] flex-shrink-0">
         <a href="http://localhost:3000">
-        <img
-          src="/Logo.png"
-          alt="Logo"
-          className="w-full h-full object-contain ml-4"
-        />
+        <h1 className="text-blue-600 text-3xl font-extrabold text-center lg:text-left">
+            MORENT
+          </h1>
         </a>
       </div>
 
       {/* Search */}
       <div className="flex items-center gap-2 w-full max-w-[492px] h-[44px] border rounded-full px-3 mt-4 ml-1 md:mt-0 md:flex-1 md:mr-8">
-        <img
+        <Image
           src="/search-normal.png"
           alt="Search"
+          width={44}
+          height={44}
           className="w-[20px] h-[20px]"
         />
         <input
@@ -27,32 +27,38 @@ export default function Header() {
           placeholder="Search something here"
           className="flex-1 bg-transparent border-none outline-none text-sm px-2 w-[492px] h-[44px]"
         />
-        <img src="/filter.png" alt="Filter" className="w-[20px] h-[20px]" />
+        <Image src="/filter.png" alt="Filter" width={44} height={44}className="w-[20px] h-[20px]" />
       </div>
 
       {/* Profile Icons */}
       <div className="flex items-center space-x-4 mt-4 md:mt-0">
         <a href="#">
-          <img src="/Like.png" alt="Like" className="w-[36px] h-[36px]" />
+          <Image src="/Like.png" alt="Like" width={44} height={44} className="w-[36px] h-[36px]" />
         </a>
         <a href="#">
-          <img
+          <Image
             src="/Notification.png"
             alt="Notification"
+            width={44}
+          height={44}
             className="w-[36px] h-[36px]"
           />
         </a>
         <a href="#">
-          <img
+          <Image
             src="/Settings.png"
             alt="Settings"
+            width={44}
+          height={44}
             className="w-[36px] h-[36px]"
           />
         </a>
         <a href="/User">
-          <img
+          <Image
             src="/profile.png"
             alt="profile"
+            width={44}
+          height={44}
             className="w-[44px] h-44px rounded-full"
           />
         </a>
