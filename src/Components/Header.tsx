f829362d964ae79401b81a642361067c71eddf09
muzammildha <info.allmaster@gmail.com>
2025-01-27 13:11:49 +0500

    template 7

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,15 +7,12 @@ export default function Header() {
       {/* Logo */}
       <div className="w-[120px] h-[40px] flex-shrink-0">
         <a href="/">
-        <Image
-          src="/Logo.png"
-          alt="Logo"
-          width={44}
-          height={44}
-          className="w-full h-full object-contain ml-4"
-        />
+        <h1 className="text-blue-600 text-3xl font-extrabold text-center lg:text-left">
+            MORENT
+          </h1>
         </a>
       </div>
+
 
       {/* Search */}
       <div className="flex items-center gap-2 w-full max-w-[492px] h-[44px] border rounded-full px-3 mt-4 ml-1 md:mt-0 md:flex-1 md:mr-8">
